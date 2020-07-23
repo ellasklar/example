df3f4a81e93519418d3ccb7214323da24b35d5c9
Ella Sklar <esklar22@gmail.com>
2020-07-23 10:32:25 -0400

    Initial Commit

---
+++ final bio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  final bio
+//
+//  Created by Apple on 7/23/20.
+//  Copyright © 2020 Ella Sklar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ final bioTests/final_bioTests.swift
@@ -0,0 +1,34 @@
+//
+//  final_bioTests.swift
+//  final bioTests
+//
+//  Created by Apple on 7/23/20.
+//  Copyright © 2020 Ella Sklar. All rights reserved.
+//
+
+import XCTest
+@testable import final_bio
+
+class final_bioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
